7eac7ddd17ae44e10f90fa9a375593d91842d939
takebul <takebulislam@gmail.com>
2026-06-10 00:58:00 +0600

    added tutorDetails page

--- src/app/(main)/tutorDetails/[id]/page.jsx
+++ src/app/(main)/tutorDetails/[id]/page.jsx
@@ -1,22 +1,75 @@
+import { tutorsSingleDataFetching } from "@/lib/data";
+import BookSessionBtn from "@/ui/BookSessionBtn";
 import Image from "next/image";
 import React from "react";
 
-const TutorsBookingPage = () => {
+const TutorsBookingPage = async ({ params }) => {
+  const { id } = await params;
+  const res = await fetch(`http://localhost:8541/tutors/${id}`);
+  const tutor = await res.json();
+
+  const {
+    tutorName,
+    tutorImage,
+    subjectCategory,
+    sessionStartDate,
+    remainingSlots,
+    mode,
+    location,
+    institution,
+    hourlyFee,
+    experience,
+    availableTimeSlot,
+  } = tutor;
+
   return (
     <div className="justify-items-center my-10">
-      <div className="flex gap-2 items-center">
+      <div className="flex gap-8 items-center border border-slate-200 p-4 shadow">
         <div>
           <Image
-            src={
-              "https://media.gettyimages.com/id/1649071923/photo/college-professor-tutors-student-after-class.jpg?s=612x612&w=0&k=20&c=Rp1AM6pOAwgH7ySTHjQUxv4VI6SiHjnL_zLt2u4o5S4="
-            }
+            className="w-full"
+            src={tutorImage}
             alt={"tutorName"}
-            width={300}
+            width={3000}
             height={300}
           />
         </div>
-        <div>
-          <p>Mosaddek</p>
+        <div className="space-y-4">
+          <h4 className="text-2xl font-bold">{tutorName}</h4>
+          <p className="text-xl text-muted font-semibold">{subjectCategory}</p>
+          <p>
+            <span className="font-semibold">Institution: </span>
+            {institution}
+          </p>
+          <p>
+            <span className="font-semibold">Experience: </span>
+            {experience}
+          </p>
+          <p>
+            <span className="font-semibold">Location: </span>
+            {location}
+          </p>
+          <p>
+            <span className="font-semibold">Mode: </span>
+            {mode}
+          </p>
+          <p>
+            <span className="font-semibold">Available & Time Slot: </span>
+            {availableTimeSlot}
+          </p>
+          <p>
+            <span className="font-semibold">Hourly Fee: $</span>
+            {hourlyFee}/hr
+          </p>
+          <p>
+            <span className="font-semibold">remainingSlots: </span>
+            {remainingSlots}
+          </p>
+          <p>
+            <span className="font-semibold">Session Start Date: </span>
+            {sessionStartDate}
+          </p>
+          <BookSessionBtn>Book Session</BookSessionBtn>
         </div>
       </div>
     </div>
