49a9f695b5cc1918112ed68867d5df88cc92b7e2
takebul <takebulislam@gmail.com>
2026-06-17 17:10:51 +0600

    added new favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,6 +13,14 @@ const poppins = Poppins({
 export const metadata = {
   title: "Tutor Book",
   description: "The Best Tutor Booking Platform in Bangladesh",
+  icons: {
+    icon: [
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({ children }) {
